d985019fb8218706b11ed46d2b226ad02e97e96a
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-03-26 17:03:09 -0300

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,12 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://nachoxt17.github.io/ignacioceaglio-portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          D$t0r@g3
+          D$t0r@g3 - Decentralized File Storage Platform with Blockchain and I.P.F.S
         </a>
         <ul className="navbar-nav px-3">
           <b className='text-white'>{'0x0'}</b>
