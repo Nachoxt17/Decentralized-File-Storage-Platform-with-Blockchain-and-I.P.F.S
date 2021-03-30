7ed399f5086601b4bf11bc4e429d436301353180
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-03-30 18:24:49 -0300

    2nd Commit

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,50 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
+  string public name = "DStorage";
+  //+-Number of files:_
+  //+-Mapping fileId=>Struct:_
+  mapping(uint => File) public files;
 
-  // Struct
+  //+-Struct:_
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
-
-  // Event
+  //+-Event:_
 
   constructor() public {
   }
 
-  // Upload File function
+  //+-Upload File function:_
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
 
-    // Make sure the file hash exists
+    files[1] = File(1, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
 
-    // Make sure file type exists
+    //+-Make sure the file hash exists:_
 
-    // Make sure file description exists
+    //+-Make sure file type exists:_
 
-    // Make sure file fileName exists
+    //+-Make sure file description exists:_
 
-    // Make sure uploader address exists
+    //+-Make sure file fileName exists:_
 
-    // Make sure file size is more than 0
+    //+-Make sure uploader address exists:_
+
+    //+-Make sure file size is more than 0:_
 
 
-    // Increment file id
+    //+-Increment file id:_
 
-    // Add File to the contract
+    //+-Add File to the contract:_
 
-    // Trigger an event
-
+    //+-Trigger an event:_
+  }
 }
